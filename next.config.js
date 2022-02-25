65690f0632f9e46467f9dcd8664686b48bd16f64
Israfil Miya <israfilmiya120@gmail.com>
2022-02-26 00:17:49 +0600

    deleted some secret stuffs

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  /*env: {
-    NEXTAUTH_URL: "http://localhost:3000",
-    MONGODB_URI: "mongodb+srv://prctracker:Prctracker@price-tracker-cluster.abzhf.mongodb.net/price-tracker?retryWrites=true&w=majority",
-    GITHUB_ID: "500c709eba55c591ee49",
-    GITHUB_SECRET: "853948d3bdb08616dc46aed7001faaece792e5a3",
-    FACEBOOK_CLIENT_ID: 2939024153019552,
-    FACEBOOK_CLIENT_SECRET: "4c3144d85ae7a7434d10054f8ef565ec",
-    DISCORD_CLIENT_ID: 945712222611599381,
-    DISCORD_CLIENT_SECRET: "tjpZxmSTBbbE4Dl_HvByCBH0AkPNY06y",
-    SECRET: "KIXarH/Ia+oZ1V0Sq5vYQfDhLaaX8rFCnc4jB2TWlLI="
-  }*/
 }
