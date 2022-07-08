3c2ae3a3473b03b315b3715260e35850b70124f4
GitPro10 <israfilmiya120@gmail.com>
2022-07-08 13:48:04 +0600

    First push

---
+++ email_html_tamplate.js
@@ -0,0 +1,54 @@
+export default function htmlTamplate(users_name,items_name, items_curr_price, items_wanted_price, website_name, items_uri, currency, items_prev_price){
+  return (`
+  <!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+</head>
+<body class="container" style="font-family: 'Courier New', monospace;">
+  <h4 class="heading" style="text-align: center;padding: 2rem;background-color: #070015;border-radius: 5px;color: #ececec;">
+    Price Tracker Update
+  </h4>
+  <div>
+    <div class="heading2" style="text-align: center;padding: 2rem;background-color: #070015;border-radius: 5px;color: #ececec;">
+      <p class="subHeading">
+        Hello ${users_name}! One of your monitored item's price got down.
+      </p>
+      <small class="tag">You may interest to buy it now!</small>
+    </div>
+    <br>
+    <div class="list" style="padding: 2rem;background-color: #070015;border-radius: 5px;color: #ececec;line-height: 6px;word-wrap: break-word;">
+      <h5>
+        <span class="field">Item's name: </span>
+        <span class="query" style="font-weight: normal;color: #E30B5C;">${items_name.substring(0,23)+"…"}</span>
+      </h5>
+      <h5>
+        <span class="field">Item's wanted price: </span>
+        <span class="query" style="font-weight: normal;color: #E30B5C;">${items_wanted_price+" "+currency}</span>
+      </h5>
+      <h5>
+        <span class="field">Item's previous price: </span>
+        <span class="query" style="font-weight: normal;color: #E30B5C;">${items_prev_price+" "+currency}</span>
+      </h5>
+      <h5>
+        <span class="field">Item's current price: </span>
+        <span class="query" style="font-weight: normal;color: #E30B5C;">${items_curr_price+" "+currency}</span>
+      </h5>
+      <h5>
+        <span class="field">Dropped: </span>
+        <span class="query" style="font-weight: normal;color: #E30B5C;">${parseFloat(items_prev_price-items_curr_price).toFixed(2)+" "+currency}</span>
+      </h5>
+      <h5>
+        <span class="field">Website: </span>
+        <span class="query" style="font-weight: normal;color: #E30B5C;">${website_name}</span>
+      </h5>
+      <h5>
+        <span class="field">Link: </span>
+        <span class="query" style="font-weight: normal;color: #E30B5C;"><a href=${items_uri} style="text-decoration: none;color: rgb(65, 105, 225);">${items_uri.substring(0,25)+"…"}</span>
+      </h5>
+    </div>
+  </div>
+</body>
+</html>
+  `)
+}
---
+++ index.js
@@ -0,0 +1,108 @@
+import getprice from "eshop-scraper"
+import * as dotenv from 'dotenv'
+dotenv.config()
+import nodemailer from "nodemailer"
+import db from "./db.js"
+import htmlTamplate from "./email_html_tamplate.js"
+//
+//
+var counter = 0;
+(async function price_tracker() {
+  counter++
+  const data = await db.Item.find()
+  data.map(async (item, i) => {
+    //
+    const UserData = await db.User.findById(item.User_ID)
+    //
+    var item_name = item.item_name
+    var curr_price = item.curr_price
+    let prev_price = item.curr_price
+    let price_wanted = item.price_wanted
+    var item_uri = item.item_uri
+    let currency = "USD"
+    var website = item.website
+    let email = UserData.email
+    let name = UserData.name
+    //
+    const itemsCurrentInfo = await getprice(item_uri, currency)
+    //
+    if (itemsCurrentInfo.price && !itemsCurrentInfo.IsError) {
+      //
+      if (itemsCurrentInfo.price != curr_price) {
+        //
+        let newData = await db.Item.findByIdAndUpdate(item._id, {
+          curr_price: itemsCurrentInfo.price,
+          item_name: itemsCurrentInfo.name,
+          website: itemsCurrentInfo.site
+        }, {
+          new: true
+        })
+        //
+        const changeObj = {
+          serial_no: i,
+          change: "Item's new price"
+        }
+        let finalData = []
+        finalData.push(newData)
+        finalData.push(changeObj)
+        console.log(finalData)
+        //
+        curr_price = newData.curr_price
+        item_name = newData.item_name
+        website = newData.website
+        //
+        if (itemsCurrentInfo.price <= price_wanted) {
+          // sends email informing the user that price is lower than his wanted price
+          let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            host: 'smtp.gmail.com',
+            auth: {
+              user: process.env.EMAIL,
+              pass: process.env.PASSWORD,
+            },
+          });
+          //
+          let info = await transporter.sendMail({
+            from: '"Price Tracker" '+'<'+process.env.EMAIL+'>',
+            to: email,
+            subject: 'Price Tracker – '+item_name,
+            html: htmlTamplate(name, item_name, itemsCurrentInfo.price, price_wanted, website, item_uri, currency, prev_price),
+          });
+          console.log({
+            email_sent: "An email has been sent to "+email
+          })
+        }
+        //
+        return;
+      }
+      //
+    } else if (itemsCurrentInfo.IsError && item_uri != "https://price-tracker-ivory.vercel.app/unknown-site") {
+      //
+      let newData = await db.Item.findByIdAndUpdate(item._id, {
+        item_uri: "https://price-tracker-ivory.vercel.app/unknown-site"
+      }, {
+        new: true
+      })
+      //
+      const changeObj = {
+        serial_no: i,
+        change: "Unknown website error"
+      }
+      let finalData = []
+      finalData.push(newData)
+      finalData.push(changeObj)
+      console.log(finalData)
+      //
+      item_uri = newData.item_uri
+      //
+    }
+    //
+  })
+  //
+  console.log({
+    info: "Loop done!",
+    loop_no: counter,
+  })
+  setTimeout(price_tracker,
+    process.env.INTERVAL_TIME_IN_SEC*1000);
+})()
