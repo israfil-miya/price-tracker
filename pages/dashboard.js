f53cfb70a75a752954549bcd4d628db36eba5ca8
GitPro10 <israfilmiya120@gmail.com>
2022-07-09 13:16:20 +0600

    Fixes

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -19,11 +19,11 @@ export async function getServerSideProps(context) {
       },
     }
   }
-  const res = await fetch(
+  const res2 = await fetch(
     'https://price-tracker-ivory.vercel.app/api/user_confg',
   )
-  const data = await res.json()
-  const datasreturn2 = JSON.parse(JSON.stringify(data.configs))
+  const data2 = await res2.json()
+  const datasreturn2 = JSON.parse(JSON.stringify(data2.configs))
 
   const rawres = await fetch(
     'https://price-tracker-ivory.vercel.app/api/items',
