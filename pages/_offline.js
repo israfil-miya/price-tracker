2dcace3f3e57443fb0d1e4e07957fa693a4cfeb1
Israfil Miya <israfilmiya120@gmail.com>
2022-07-28 03:46:22 +0600

    Update _offline.js

--- pages/_offline.js
+++ pages/_offline.js
@@ -1,15 +1,10 @@
 export default function OfllinePage() {
   return (
     <>
-      <div className="container text-center my-5 align-items-center justify-content-center">
+      <div className="text-center my-5">
         <h3>Whoops!</h3>
         <p>Looks like your internet connection is gone.</p>
       </div>
-      <style jsx>{`
-      .container {
-        height: 100%
-      }
-      `}</style>
     </>
   )
 }
